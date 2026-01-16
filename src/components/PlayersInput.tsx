42499cb52ea58b7781b90fe91485081f07eb09fc
Danila Andreev <qq@daanqq.ru>
2026-01-16 22:09:30 +0500

    update instructions

--- src/components/PlayersInput.tsx
+++ src/components/PlayersInput.tsx
@@ -1,7 +1,6 @@
 import i18next from 'i18next'
 import { motion } from 'motion/react'
 
-import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -15,8 +14,7 @@ import { Input } from './ui/input'
 const PlayersInput = () => {
   const {
     stringToParse,
-    setStringToParse,
-    copyCommand
+    setStringToParse
   } = useStore()
 
   const placeholder = i18next.t('placeholder')
@@ -30,17 +28,14 @@ const PlayersInput = () => {
       <Card>
         <CardHeader>
           <CardTitle>
-            {i18next.t('enterString')}
-            <Button
-              variant="outline"
-              className="px-1 h-6 mx-1 rounded-sm"
-              onClick={copyCommand}
-            >
-              voice_show_mute
-            </Button>
-            {i18next.t('enterStringEnd')}
+            {i18next.t('instructionsTitle')}
           </CardTitle>
-          <CardDescription>{i18next.t('enterStringDesc')}</CardDescription>
+          <CardDescription>
+            {i18next.t('instructionsStep1')}<br />
+            {i18next.t('instructionsStep2')}<br />
+            {i18next.t('instructionsStep3')}<br />
+            {i18next.t('instructionsStep4')}
+          </CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col gap-2">
           <Input
