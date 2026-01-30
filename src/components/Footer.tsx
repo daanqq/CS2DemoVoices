fa6445208700c61f65b8173fbcd3c02ccb8ed0ae
Danila Andreev <qq@daanqq.ru>
2026-01-31 00:05:59 +0500

    remove tailwind merge for build size decrease

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,17 +6,17 @@ const Footer = () => {
   const { i18n } = useStore();
 
   return (
-    <Card className="flex text-sm px-3 py-2 border border-b-0 rounded-xl rounded-b-none">
-      <CardContent className="flex p-0 gap-16">
+    <Card className="flex text-sm px-3! py-2! border border-b-0 rounded-xl rounded-b-none">
+      <CardContent className="flex p-0! gap-16">
         <div className="items-center">
           <p className="inline">{i18n.t("madeBy")}</p>
-          <Button variant="link" className="p-1">
+          <Button variant="link" className="p-1!">
             <a href="https://github.com/daanqq" target="_blank" rel="noopener noreferrer">
               @daanqq
             </a>
           </Button>
         </div>
-        <Button variant="link" className="p-1">
+        <Button variant="link" className="p-1!">
           <a
             href="https://github.com/daanqq/CS2DemoVoices"
             target="_blank"
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,9 @@
 import { cva } from "class-variance-authority";
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+// import { twMerge } from "tailwind-merge";
 
-export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+export const cn = (...inputs: ClassValue[]) => clsx(inputs);
+// export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
 export const buttonVariants = cva(
   "select-none inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
